b11c12fe29e2c2436d5b2408e71746915bf47350
Raymond <773561801@qq.com>
2020-09-19 12:10:26 +0800

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,20 @@ pub fn say(s: &str) -> String {
   let r = String::from("hello ");
   return r + s;
 }
+
+pub fn main(s: &str) -> String {
+  let mut y: i32 = 1;
+  while y < 10 {
+    let mut x: i32 = 1;
+    while x < 10 {
+      print!("{}*{}={:?} \t", x, y, x * y); // 同行内打印
+      x += 1;
+      if x > y {
+        break;
+      }
+    }
+    println!(""); //换行
+    y += 1;
+  }
+}
+
