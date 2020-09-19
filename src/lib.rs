c3d1df6809a3029afcc555543c93ab5b56cdea6c
Raymond <773561801@qq.com>
2020-09-19 12:13:35 +0800

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,8 @@ pub fn main(s: &str) -> String {
   while y < 10 {
     let mut x: i32 = 1;
     while x < 10 {
-      print!("{}*{}={:?} \t", x, y, x * y); // 同行内打印
+      let r = String::from("{}*{}={:?} \t", x, y, x * y);
+      print!(r); // 同行内打印
       x += 1;
       if x > y {
         break;
